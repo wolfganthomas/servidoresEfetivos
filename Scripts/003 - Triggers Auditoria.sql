288752912b7546d4415f63a21a80647782c7c385
Thomas Wolfgan <wolfganthomas@gmail.com>
2024-09-20 18:47:00 -0300

    Continuidade no desenvolvimento da trigger de auditoria da tabela de servidor

--- Scripts/003 - Triggers Auditoria.sql
+++ Scripts/003 - Triggers Auditoria.sql
@@ -56,16 +56,39 @@ BEGIN
 							FROM INSERTED I
 						)
 
-			EXEC InserirAuditoria @TP_OPERACAO,'SERVIDOR',@DS_OPERACAO
+			
 		END;
 
+	IF @TP_OPERACAO='U'
+		BEGIN
+			SET @DS_OPERACAO=(CONCAT(	
+										'Alteração de cadastro de servidor. ** Dados antes a alteração:'
+										,(SELECT CONCAT(
+											' ID_SERVIDOR: ',D.ID_SERVIDOR
+											,'; CD_SIAPE: ',D.CD_SIAPE
+											,'; NM_SERVIDOR: ',D.NM_SERVIDOR
+											,'; TP_SEXO: ',D.TP_SEXO
+											,'; DT_NASCIMENTO: ',D.DT_NASCIMENTO
+											,'; NR_CPF: ',D.NR_CPF
+											,'; FG_REGISTRO_ATIVO: ',D.FG_REGISTRO_ATIVO)
+											FROM DELETED D)
+										,'** Dados após a alteração:'
+										,(SELECT CONCAT(
+											' ID_SERVIDOR: ',I.ID_SERVIDOR
+											,'; CD_SIAPE: ',I.CD_SIAPE
+											,'; NM_SERVIDOR: ',I.NM_SERVIDOR
+											,'; TP_SEXO: ',I.TP_SEXO
+											,'; DT_NASCIMENTO: ',I.DT_NASCIMENTO
+											,'; NR_CPF: ',I.NR_CPF
+											,'; FG_REGISTRO_ATIVO: ',I.FG_REGISTRO_ATIVO)
+											FROM INSERTED I)
+										)
+										
+							)
+		END;
+
+	EXEC InserirAuditoria @TP_OPERACAO,'SERVIDOR',@DS_OPERACAO
 END
 GO
 
 
-
-SELECT * FROM SERVIDOR
-
-INSERT INTO SERVIDOR VALUES (1,'SERVIDOR TESTE 3','M','19940101','11111111111',1)
-
-SELECT * FROM AUDITORIA

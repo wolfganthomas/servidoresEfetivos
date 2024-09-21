9adb87f07324c8385e70cb0467376aa6f1672e6c
wolfganthomas <wolfganthomas@gmail.com>
2024-09-21 09:28:42 -0300

    Incluído cabeçalho do script e comando para teste de inserção

--- Scripts/005 - Procedure InserirServidor.sql
+++ Scripts/005 - Procedure InserirServidor.sql
@@ -1,6 +1,15 @@
+/*========================================================================*/
+/*  Autor do Script: Thomas Wolfgan			                  */
+/*  Data de criação: 21/09/2024				                  */
+/*  Finalidade do script:						  */
+/*     Criar procedure para inserção de servidor        		  */ 
+/*  Sumário do Script:                                                    */
+/*     1. Cria a procedure InserirServidor                                */
+/*========================================================================*/
+
 
 /*=====================================================*/
-/* -- Procedure utilizada para cadastro de servidor	 --*/
+/* -- Procedure utilizada para cadastro de servidor  --*/
 /*=====================================================*/
 
 USE SERVIDOR_EFETIVO
@@ -29,3 +38,26 @@ END
 GO
 
 GRANT EXECUTE ON InserirServidor TO USR_SERVIDOREFETIVO
+
+/*=====================================================*/
+/* -- Comando para testar a procedure                --*/
+/*=====================================================*/
+
+/*
+DECLARE @CD_SIAPE int= 15487
+DECLARE @NM_SERVIDOR varchar(100)='Nome do servidor'
+DECLARE @TP_SEXO char(1)='M' -- Valores aceitos 'M' ou 'F', para masculino e feminino, respectivamente
+DECLARE @DT_NASCIMENTO date='1990-01-10'
+DECLARE @NR_CPF varchar(11)= '00000000000'
+DECLARE @FG_REGISTRO_ATIVO bit=1 -- Valores aceitos 1 ou 0, para ativo e inativo, respectivamente
+
+EXECUTE InserirServidor
+   @CD_SIAPE
+  ,@NM_SERVIDOR
+  ,@TP_SEXO
+  ,@DT_NASCIMENTO
+  ,@NR_CPF
+  ,@FG_REGISTRO_ATIVO
+GO
+
+*/

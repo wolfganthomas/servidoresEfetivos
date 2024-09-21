5f6b0e018d246f62bdd1f017b1f9f93eb1583d99
wolfganthomas <wolfganthomas@gmail.com>
2024-09-21 09:16:11 -0300

    Script renomeado e cabecalho incluído

---
+++ Scripts/000- Database e usuários.sql
@@ -0,0 +1,28 @@
+/*========================================================================*/
+/*  Autor do Script: Thomas Wolfgan						                            */
+/*  Data de criação: 21/09/2024				                                    */
+/*  Finalidade do script:											                            */
+/*     Criar database e usuários que serão utilizados                     */ 
+/*  Sumário do Script:                                                    */
+/*     1. Cria a database SERVIDOR_EFETIVO                                */
+/*     2. Cria o usuário de usr_servidorefetivo                           */
+/*     3. Cria o usuário de usr_servidorefetivo_bi                        */
+/*========================================================================*/
+
+USE MASTER
+GO
+CREATE DATABASE SERVIDOR_EFETIVO
+
+CREATE LOGIN usr_servidorefetivo WITH PASSWORD='usr_servidorefetivo', CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+
+USE SERVIDOR_EFETIVO
+GO
+CREATE USER usr_servidorefetivo FOR LOGIN usr_servidorefetivo
+GO
+
+CREATE LOGIN usr_servidorefetivo_bi WITH PASSWORD='usr_servidorefetivo_bi', CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+
+USE SERVIDOR_EFETIVO
+GO
+CREATE USER usr_servidorefetivo_bi FOR LOGIN usr_servidorefetivo_bi
+GO

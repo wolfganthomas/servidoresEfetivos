20283782635da69a4dbd17ec1a6f4ba3a8886896
wolfganthomas <wolfganthomas@gmail.com>
2024-09-21 09:51:25 -0300

    Incluído comando para remoção do usuário caso já existe
    A finalidade é facilitar o teste do script.

--- Scripts/000- Database e usuários.sql
+++ Scripts/000- Database e usuários.sql
@@ -13,16 +13,34 @@ USE MASTER
 GO
 CREATE DATABASE SERVIDOR_EFETIVO
 
+
+IF EXISTS (SELECT 1 FROM SYS.SYSLOGINS WHERE NAME='usr_servidorefetivo')
+	DROP LOGIN usr_servidorefetivo
+	
+GO
 CREATE LOGIN usr_servidorefetivo WITH PASSWORD='usr_servidorefetivo', CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
 
 USE SERVIDOR_EFETIVO
+GO
+IF EXISTS(SELECT 1 FROM SYS.sysusers WHERE NAME='usr_servidorefetivo')
+	DROP USER usr_servidorefetivo
 GO
 CREATE USER usr_servidorefetivo FOR LOGIN usr_servidorefetivo
 GO
 
+USE MASTER
+GO
+IF EXISTS (SELECT 1 FROM SYS.SYSLOGINS WHERE NAME='usr_servidorefetivo_bi')
+	DROP LOGIN usr_servidorefetivo_bi
+	
 CREATE LOGIN usr_servidorefetivo_bi WITH PASSWORD='usr_servidorefetivo_bi', CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
 
 USE SERVIDOR_EFETIVO
 GO
+IF EXISTS(SELECT 1 FROM SYS.sysusers WHERE NAME='usr_servidorefetivo_bi')
+	DROP USER usr_servidorefetivo_bi
+GO
+
 CREATE USER usr_servidorefetivo_bi FOR LOGIN usr_servidorefetivo_bi
 GO

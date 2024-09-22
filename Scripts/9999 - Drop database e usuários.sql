69feff7e1b3d7a04c43e872fdb62f4537d4859fd
Thomas Wolfgan <wolfganthomas@gmail.com>
2024-09-21 23:05:12 -0300

    Criado script para remoção da base de dados e ajustes nos relatórios
    Esse script é apenas para remoção da base de dados caso surja a necessidade de recriar o ambiente do zero para testar os scripts de criação, por exemplo.

---
+++ Scripts/9999 - Drop database e usuários.sql
@@ -0,0 +1,22 @@
+/*========================================================================*/
+/*  Autor do Script: Thomas Wolfgan					                      */
+/*  Data de criação: 21/09/2024				                              */
+/*  Finalidade do script:						                          */
+/*     Possibilitar remoção da base de dados. 				              */ 
+/*     Script criado para facilitar testes, só deve ser utilizado se	  */ 
+/*     desejar recriar o ambiente do zero                        		  */ 
+/*  Sumário do Script:                                                    */
+/*     1. Remove a base de dados SERVIDOR_EFETIVO	    		          */
+/*========================================================================*/
+
+
+USE MASTER
+DROP DATABASE SERVIDOR_EFETIVO
+
+-- ALTERA O BANCO DE DADOS PARA SINGLE_USER, FORÇANDO A DESCONEXÃO DE TODOS OS USUÁRIOS
+ALTER DATABASE SERVIDOR_EFETIVO 
+SET SINGLE_USER 
+WITH ROLLBACK IMMEDIATE;
+
+-- EXCLUI O BANCO DE DADOS
+DROP DATABASE SERVIDOR_EFETIVO;
